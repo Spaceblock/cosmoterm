84fbb35a9d4477202c54064c8c1acda67fc6b070
mb6ockatf <mdddmmmm@yandex.ru>
2023-11-19 00:30:05 +0300

    feat: error handling & docker

--- src/app.py
+++ src/app.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 from sys import argv
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
+from dataclasses import dataclass
+import werkzeug
 from waitress import serve
 
 
@@ -8,11 +10,45 @@ app = Flask(__name__)
 app.secret_key = argv[1].encode()
 DEBUG = True
 
+@dataclass
+class ErrorData:
+    code: int
+    message: str
+
+
+@app.route("/index")
+@app.route("/index.html")
+@app.route("/index.htm")
+@app.route("/main")
+def redirect_to_root():
+    return redirect("/", code=302)
+
 
 @app.route("/")
 def home():
     return render_template("home.html")
 
+
+@app.route("/login")
+def login():
+    return "nothing here yet!"
+
+
+@app.route("/signin")
+def signin():
+    return "nothing here yet!"
+
+def handle_bad_request(e):
+    data = ErrorData(400, "bad request")
+    return render_template("error.html", context=data), 400
+
+app.register_error_handler(400, handle_bad_request)
+
+def handle_missing_page(e):
+    data = ErrorData(404, "page not found")
+    return render_template("error.html", context=data), 404
+
+app.register_error_handler(404, handle_missing_page)
 
 if DEBUG:
     app.run(debug=True)
